1ae4c4bacf8f9bebf5e614fe32cd06a5d8e0e753
perkles <maikosilvae@gmail.com>
2022-04-27 01:54:56 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        long N = leitor.nextLong();
+        long L = leitor.nextLong();
+        long P = N * L;
+        System.out.println(P);
+    }
+
+}
